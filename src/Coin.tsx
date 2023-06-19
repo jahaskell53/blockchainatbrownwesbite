553237e3d99a17037564e0a364dcc80ea52536a8
Jakobi Haskell <jakobi_haskell@brown.edu>
2023-06-19 18:55:52 -0400

    make this change to make model larger and bg transparent

--- src/Coin.tsx
+++ src/Coin.tsx
@@ -5,9 +5,9 @@ import { useLoader } from '@react-three/fiber';
 import { OrbitControls, Html, Loader } from "@react-three/drei";
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, 'https://blockchainatbrown.com/coin.glb')
+  const gltf = useLoader(GLTFLoader, '/path/to/model.glb')
 
-  return <primitive object={gltf.scene} dispose={null} />
+  return <primitive object={gltf.scene} scale={3} dispose={null} />
 }
 
 const Loading = () => (
@@ -17,14 +17,16 @@ const Loading = () => (
 );
 
 const App = () => (
-  <Canvas style={{ background: 'black' }} camera={{ position: [0, 0, 10] }}>
-    <Suspense fallback={<Loading />}>
-      <OrbitControls />
-      <Model />
-    </Suspense>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-  </Canvas>
+  <div style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: -1 }}>
+    <Canvas style={{ background: 'transparent' }} camera={{ position: [0, 0, 15], fov: 75 }}>
+      <Suspense fallback={<Loading />}>
+        <OrbitControls />
+        <Model />
+      </Suspense>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+    </Canvas>
+  </div>
 );
 
 export default App;
