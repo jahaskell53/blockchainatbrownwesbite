20014797d57daf4127512f47807987b2c10be9e0
Jakobi Haskell <jakobi_haskell@brown.edu>
2023-06-19 20:14:27 -0400

    adds typing

--- src/Coin.tsx
+++ src/Coin.tsx
@@ -1,13 +1,17 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
 import { OrbitControls, Html, Loader } from "@react-three/drei";
+import { ThreeElements, useFrame } from 'react-three-fiber';
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, 'https://blockchainatbrown.com/coin.glb')
+  const gltf = useLoader(GLTFLoader, 'https://blockchainatbrown.com/coin.glb');
+  const mesh = useRef<ThreeElements["primitive"]>()
 
-  return <primitive object={gltf.scene} dispose={null} scale={[10, 10, 10]} rotation={[0, Math.PI + Math.PI / 5, 0]} />
+  useFrame((state, delta) => (mesh.current!.rotation.y += delta * 0.5))
+
+  return <primitive ref={mesh} object={gltf.scene} dispose={null} scale={[10, 10, 10]} rotation={[0, Math.PI + Math.PI / 5, 0]} />
 }
 
 const Loading = () => (
